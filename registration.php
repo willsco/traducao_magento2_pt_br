8ff0455925db0df2f6d006d3cde8f0e74544915e
willsco <williamgambas@gmail.com>
2022-08-02 09:57:34 -0300

    fix: change vendor name

--- registration.php
+++ registration.php
@@ -3,4 +3,4 @@ declare(strict_types=1);
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'rafaelcg_pt_br', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::LANGUAGE, 'modesto_pt_br', __DIR__);
